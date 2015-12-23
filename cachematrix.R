a57b6d67b583ab52ed31df18325c1408c7cb4795
ywanggithub <yinyan.wang@nsn.com>
2015-12-22 21:29:21 -0600

    Revert "More typos"
    This reverts commit 7f657dd22ac20d22698c53b23f0057e1a12c09b7.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix This function creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
+## Usage: a<-makeCacheMatrix(x). output a is a list of four functions 
+## getm, setm, getinverse, setinverse, input x is an invertible matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    minv <- NULL
+    setm <- function(y) {
+        x <<- y
+        minv <<- NULL
+    }
+    getm <- function() x
+    setinverse <- function(solve) minv <<- solve
+    getinverse <- function() minv
+    list(setm = setm, getm = getm,
+         setinverse = setinverse,
+         getinverse = getinverse)  
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve the 
+## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    minv <- x$getinverse()
+    if(!is.null(minv)) {
+        message("getting cached data")
+        return(minv)
+    }
+    data <- x$getm()
+    minv <- solve(data, ...)
+    x$setinverse(minv)
+    minv
 }
